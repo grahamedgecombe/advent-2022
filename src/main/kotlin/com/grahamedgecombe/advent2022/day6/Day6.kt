224533626eb7025ea6674b2e9580eb5f52d41095
Graham Edgecombe <gpe@grahamedgecombe.com>
2022-12-06 19:07:45 +0000

    Add day 6 part 2 solution

--- src/main/kotlin/com/grahamedgecombe/advent2022/day6/Day6.kt
+++ src/main/kotlin/com/grahamedgecombe/advent2022/day6/Day6.kt
@@ -9,10 +9,18 @@ object Day6 : Puzzle<String>(6) {
     }
 
     override fun solvePart1(input: String): Int {
-        for (i in 4..input.length) {
-            val history = input.substring(i - 4, i)
+        return solve(input, 4)
+    }
 
-            if (history.toSet().size == 4) {
+    override fun solvePart2(input: String): Int {
+        return solve(input, 14)
+    }
+
+    private fun solve(input: String, n: Int): Int {
+        for (i in n..input.length) {
+            val history = input.substring(i - n, i)
+
+            if (history.toSet().size == n) {
                 return i
             }
         }
--- src/test/kotlin/com/grahamedgecombe/advent2022/day6/Day6Test.kt
+++ src/test/kotlin/com/grahamedgecombe/advent2022/day6/Day6Test.kt
@@ -14,6 +14,16 @@ class Day6Test {
         assertEquals(1816, Day6.solvePart1(PROD_INPUT))
     }
 
+    @Test
+    fun testPart2() {
+        assertEquals(19, Day6.solvePart2(TEST_INPUT_1))
+        assertEquals(23, Day6.solvePart2(TEST_INPUT_2))
+        assertEquals(23, Day6.solvePart2(TEST_INPUT_3))
+        assertEquals(29, Day6.solvePart2(TEST_INPUT_4))
+        assertEquals(26, Day6.solvePart2(TEST_INPUT_5))
+        assertEquals(2625, Day6.solvePart2(PROD_INPUT))
+    }
+
     private companion object {
         private const val TEST_INPUT_1 = "mjqjpqmgbljsphdztnvjfqwrcgsmlb"
         private const val TEST_INPUT_2 = "bvwbjplbgvbhsrlpgdmjqwftvncz"
